9f63b548372e5e5ac94b7e5c584ae32d158e24b8
Nathan Eror <neror@freetimestudios.com>
2012-08-15 15:06:47 -0500

    Add Check unit testing framework and a test target.

---
+++ TestEPUB3Processor/check_EPUB3.c
@@ -0,0 +1,10 @@
+//
+//  check_EPUB3.c
+//  EPUB3Processor
+//
+//  Created by Nathan Eror on 8/15/12.
+//  Copyright (c) 2012 Triple Threat Mobile Development. All rights reserved.
+//
+
+#include "EPUB3.h"
+
---
+++ TestEPUB3Processor/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TestEPUB3Processor
+//
+//  Created by Nathan Eror on 8/15/12.
+//  Copyright (c) 2012 Triple Threat Mobile Development. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
+
